059ab9a7b7a765619f95d3a8abdf2dca88cb99c6
daiqin <daiqin1046@gmail.com>
2025-07-23 17:29:54 +0800

    feature: 🔨 project init

---
+++ .eslintrc.mjs
@@ -0,0 +1,20 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  rules: {
+    'no-console': [
+      'error',
+      { allow: ['log', 'warn', 'error', 'info', 'time', 'timeEnd'] },
+    ],
+    'style/multiline-ternary': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'jsdoc/require-returns-description': 'off',
+    'ts/method-signature-style': 'off',
+    'ts/no-unsafe-function-type': 'off',
+    'no-unreachable-loop': 'off',
+    'eqeqeq': 'off',
+    'no-cond-assign': 'off',
+    'no-sequences': 'off',
+    'default-case-last': 'off',
+  },
+})

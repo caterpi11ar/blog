2f0ce06475d6fd62c6e01979cd6f7238b5c95027
caterpi11ar <daiqin1046@gmail.com>
2025-08-03 02:40:28 +0800

    feature: python note

--- src/utils/og-templates/post.tsx
+++ src/utils/og-templates/post.tsx
@@ -10,16 +10,14 @@ export default (post: CollectionEntry<"blog">) => {
       }}
     >
       <div
+        className="absolute flex justify-center"
         style={{
-          position: "absolute",
           top: "-1px",
           right: "-1px",
           border: "4px solid #000",
           background: "#ecebeb",
           opacity: "0.9",
           borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
           margin: "2.5rem",
           width: "88%",
           height: "80%",
@@ -27,61 +25,52 @@ export default (post: CollectionEntry<"blog">) => {
       />
 
       <div
+        className="flex justify-center"
         style={{
           border: "4px solid #000",
           background: "#fefbfb",
           borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
           margin: "2rem",
           width: "88%",
           height: "80%",
         }}
       >
         <div
+          className="flex-col flex justify-between"
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
             margin: "20px",
             width: "90%",
             height: "90%",
           }}
         >
           <p
+            className="overflow-hidden font-bold"
             style={{
               fontSize: 72,
-              fontWeight: "bold",
               maxHeight: "84%",
-              overflow: "hidden",
             }}
           >
             {post.data.title}
           </p>
           <div
+            className="flex justify-between w-full mb-2"
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              marginBottom: "8px",
               fontSize: 28,
             }}
           >
             <span>
               by{" "}
               <span
-                style={{
-                  color: "transparent",
-                }}
+                className="text-transparent"
               >
                 "
               </span>
-              <span style={{ overflow: "hidden", fontWeight: "bold" }}>
+              <span className="overflow-hidden font-bold">
                 {post.data.author}
               </span>
             </span>
 
-            <span style={{ overflow: "hidden", fontWeight: "bold" }}>
+            <span className="overflow-hidden font-bold">
               {SITE.title}
             </span>
           </div>
